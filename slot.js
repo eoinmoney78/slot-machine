8f7c94cb46b548ef69e1825807533f1b9629cdbd
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-08-02 16:55:07 -0400

    start styling

---
+++ slot.js
@@ -0,0 +1,18 @@
+// Configure consts
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOL_COUNT = {
+    A: 6,
+    B: 8,
+    C: 10,
+    D: 12
+};
+
+const SYMBOL_VALUES = {
+    A: 2,
+    B: 2,
+    C: 12,
+    D: 12
+};
